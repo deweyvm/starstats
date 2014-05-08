53ca654bb058a4674e3d3182382f736dce430a5b
deweyvm <deweyvm@gmail.com>
2014-05-08 14:34:42 -0400

    read from log file and read log file from config

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,34 @@
-
+{-# LANGUAGE DoAndIfThenElse #-}
 import Database.HDBC
 import Database.HDBC.ODBC
+import Control.Applicative
+import System.Directory
+import System.IO
+
+configFile :: String
+configFile = "config"
+
+readConfig :: IO String
+readConfig = do
+    exists <- not <$> doesFileExist configFile
+    if exists
+    then
+        error "file 'config' not found"
+    else do
+        config <- lines <$> readFile configFile
+        return $ processConfig config
+    where processConfig (c:_) = c
+          processConfig     _ = error "file 'config' is empty"
 
 main :: IO ()
 main = do
-    let connectionString =  "DSN=name32;Driver={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Port=3306;Database=testdb;User=root;Password=password;Option=3;"
+    logfile <- readConfig
+    contents <- lines <$> readFile logfile
+    print contents
+    {-let connectionString =  "DSN=name32;Driver={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Port=3306;Database=testdb;User=root;Password=password;Option=3;"
     let ioconn = connectODBC connectionString
     conn <- ioconn
     vals <- quickQuery conn "SELECT version();" []
-    print vals
+    print vals-}
+
 
