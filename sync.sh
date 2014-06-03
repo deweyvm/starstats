731b4bca715b8419007e7eaff1cc848e1a5ce406
deweyvm <deweyvm@gmail.com>
2014-06-03 14:32:37 -0400

    more permissions

--- sync.sh
+++ sync.sh
@@ -2,5 +2,6 @@ cp dist/build/ircdb/ircdb /usr/lib/cgi-bin
 chmod a+x /usr/lib/cgi-bin/ircdb
 cp generate.cgi /usr/lib/cgi-bin
 chmod a+r /usr/lib/cgi-bin/generate.cgi
+chown dogue:dogue /usr/lib/cgi-bin/*
 cp css.css util.js /var/www
 chmod a+r /var/www/*
