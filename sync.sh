7b6786595548569babae7cfa80344b9011621c2c
deweyvm <deweyvm@gmail.com>
2014-06-03 14:33:36 -0400

    cp -a

--- sync.sh
+++ sync.sh
@@ -1,7 +1,6 @@
-cp dist/build/ircdb/ircdb /usr/lib/cgi-bin
+cp -a dist/build/ircdb/ircdb /usr/lib/cgi-bin
 chmod a+x /usr/lib/cgi-bin/ircdb
-cp generate.cgi /usr/lib/cgi-bin
+cp -a generate.cgi /usr/lib/cgi-bin
 chmod a+r /usr/lib/cgi-bin/generate.cgi
-chown dogue:dogue /usr/lib/cgi-bin/*
-cp css.css util.js /var/www
+cp -a css.css util.js /var/www
 chmod a+r /var/www/*
