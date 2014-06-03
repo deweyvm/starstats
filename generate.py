87ddd2a08e224ebabd84e0b704a075fefbe0258d
deweyvm <deweyvm@gmail.com>
2014-06-03 13:37:16 -0400

    updated generate script

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import subprocess
-
-p = subprocess.Popen("/home/dogue/ircdb/dist/build/ircdb/ircdb",stdout=subprocess.PIPE)
+env = {'MYSQL_UNIX_PORT':'/var/run/mysqld/mysqld.sock'}
+p = subprocess.Popen(["/home/dogue/ircdb/dist/build/ircdb/ircdb", "MySql ODBC 5.1 Driver", "-g"],
+                     stdout=subprocess.PIPE,
+                     env=env)
 print ("Content-type: text/html\n\n")
 print p.stdout.read()
 p.wait()
