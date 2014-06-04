68d3ef2797ea62deac0cbe07897b07998381ff98
deweyvm <deweyvm@gmail.com>
2014-06-04 12:46:37 -0400

    parse error

--- src/Main.hs
+++ src/Main.hs
@@ -9,10 +9,10 @@ main :: IO ()
 main = do
     args <- getArgs
     if elem "-w" args
-    then let file = args !! 0
-         let repop = args !! 1 == "-rp"
-         watch file repop
-    else let driver = args !! 0
-         let chanName = args !! 1
-         let actions = if elem "-p" args then [Repopulate, Generate] else [Generate]
-         sequence_ $ doAction driver chanName <$> actions
+    then do let file = args !! 0
+            let repop = args !! 1 == "-rp"
+            watch file repop
+    else do let driver = args !! 0
+            let chanName = args !! 1
+            let actions = if elem "-p" args then [Repopulate, Generate] else [Generate]
+            sequence_ $ doAction driver chanName <$> actions
