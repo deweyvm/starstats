70dc13a4449fd0373151e9dbede34ae318543905
deweyvm <deweyvm@gmail.com>
2014-06-06 11:22:08 -0400

    formatting

--- src/StarStats/DB/Queries.hs
+++ src/StarStats/DB/Queries.hs
@@ -18,7 +18,7 @@ getUniqueNicks con =
 
 getOverallActivity :: IConnection c => c -> IO [(Int,Int)]
 getOverallActivity con = do
-    let q = "SELECT h0,  h1,  h2,  h3,   h4,  h5,\
+    let q = "SELECT h0,  h1,  h2,  h3,  h4,  h5,\
            \        h6,  h7,  h8,  h9,  h10, h11,\
            \        h12, h13, h14, h15, h16, h17,\
            \        h18, h19, h20, h21, h22, h23\
